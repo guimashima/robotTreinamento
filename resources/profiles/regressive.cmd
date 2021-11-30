2f4190afa0cb03b4d250c6923a571b5c7ac3dbde
Shima <guilherme.shima@accenture.com>
2021-11-30 09:53:10 -0300

    removendo profiles

--- resources/profiles/regressive.cmd
+++
@@ -1,6 +0,0 @@
-rmdir /s /q "./logs"
-robot -d ./logs/run -i %1 -v ENVIRONMENT:%2 -v BROWSER:%3 %4
-robot -d ./logs/rerun -R ./logs/run/output.xml -v ENVIRONMENT:%2 -v BROWSER:%3 %4
-IF exist ./logs/rerun (
-        rebot -d ./logs/combined --merge ./logs/run/output.xml ./logs/rerun/output.xml
-)
